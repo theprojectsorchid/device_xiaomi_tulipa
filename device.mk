d1ab3daedcfc00e899e3300a1263f0924ddf9506
Vivekachooz <vivekachooz@gmail.com>
2022-02-05 17:31:26 +0530

    twolip: Update Bluetooth blobs from ddv_sprout
    Signed-off-by: Vivekachooz <vivekachooz@gmail.com>
    Change-Id: I14bf551f511cd5ffb20738ef524281187cf8a21c

--- device.mk
+++ device.mk
@@ -130,7 +130,8 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
