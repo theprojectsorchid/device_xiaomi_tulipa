d4f3c8bf6cc85cecbcf6ab0f54bb8875468a13b0
Erfan Abdi <erfangplus@gmail.com>
2022-03-02 19:55:14 +0530

    twolip: Build libavservices_minijail
    * For both system and vendor
    
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>
    Change-Id: Ice148afd9f76984e516bcdc458c365dfc4e51039

--- device.mk
+++ device.mk
@@ -286,6 +286,10 @@ PRODUCT_PACKAGES += \
     android.hardware.lights-service.twolip
 
 # Media
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail.vendor
+
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(DEVICE_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
