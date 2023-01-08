6fa4f8bacd4015715d4313aefe142e485f8855b8
matheucomth <matheussouzas65@outlook.com>
2023-01-08 02:02:02 -0300

    tulip: build android.hardware.camera.provider 64 variant

--- device.mk
+++ device.mk
@@ -130,10 +130,12 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.device@3.5:64 \
-    android.hardware.camera.provider@2.4-impl:32 \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5:64 \
+    android.hardware.camera.provider@2.6:64 \
     libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0:64
 
