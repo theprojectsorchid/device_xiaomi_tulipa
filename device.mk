0bc8adfedfe47c6af093a61cd10f7fd0d17d0125
Kunmun <kunmun.devroms@gmail.com>
2023-01-09 09:03:15 -0300

    tulip: Build libstdc++ to vendor

--- device.mk
+++ device.mk
@@ -348,6 +348,10 @@ PRODUCT_PACKAGES += \
     libjson \
     libjson.vendor
 
+# QNS
+PRODUCT_PACKAGES += \
+    libstdc++.vendor
+	
 # Remove few packages
 PRODUCT_PACKAGES += \
     RemovePackages
