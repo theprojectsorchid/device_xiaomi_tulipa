5ce8b268bab2b0b222d093b3f6eb11efbfc77e9c
matheucomth <matheussouzas65@outlook.com>
2023-01-25 09:35:36 -0300

    twolip: Build 64 variant of android.hardware.camera.provider@2.4

--- device.mk
+++ device.mk
@@ -133,7 +133,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.5:64 \
-    android.hardware.camera.provider@2.4-impl:32 \
+    android.hardware.camera.provider@2.4-impl:64 \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5:64 \
     libdng_sdk.vendor \
