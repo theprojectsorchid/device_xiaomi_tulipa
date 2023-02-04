3fcdbd08e79e00a894c585d79174a12c99d03660
Rajshekar Eashwarappa <reashw@codeaurora.org>
2023-02-04 15:27:29 -0300

    tulip: Remove vibrator HAL interface
    Since we are moving to vendor vibrator HAL
    Remove the existing HIDL based vibrator HAL

--- device.mk
+++ device.mk
@@ -411,11 +411,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl:64 \
-    android.hardware.vibrator@1.0-service
-
 # Wifi
 PRODUCT_PACKAGES += \
     libminijail \
