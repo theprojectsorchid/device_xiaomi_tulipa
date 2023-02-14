d8b325acf08ba5347ec27750df41366071a66888
Kyle Harrison <khwebmail@gmail.com>
2023-02-14 12:21:44 -0300

    tulip: Don't write binary XML files
    - Android 12 now writes XML files in binary format by default. This can cause incompatibility with TWRP which can hang when attempting to read XML files e.g. /data/system/storage.xml
    
    - This commit sets the persist.sys.binary_xml property to false so that XML files are written in text format.

--- device.mk
+++ device.mk
@@ -402,6 +402,10 @@ PRODUCT_PACKAGES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
+# System
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.binary_xml=false
+    
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
