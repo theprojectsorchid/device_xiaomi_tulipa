3f34d58b9cceb1e61ad6701ac9e22827ac2d4f0f
matheucomth <matheussouzas65@outlook.com>
2023-02-20 14:17:08 -0300

    tulip: Add default camera GCamGoPrebuilt-V2

--- device.mk
+++ device.mk
@@ -135,6 +135,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5:64 \
     libdng_sdk.vendor \
+    GCamGOPrebuilt-V2 \
     vendor.qti.hardware.camera.device@1.0:64
 
 # Cgroup and task_profiles
